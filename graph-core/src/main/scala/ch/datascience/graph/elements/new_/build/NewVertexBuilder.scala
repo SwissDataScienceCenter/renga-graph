63d643e81cddb9a5babf9a6face96df16a80cc77
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-07-05 16:43:14 +0200

    can now add meta-properties simply

--- graph-core/src/main/scala/ch/datascience/graph/elements/new_/build/NewVertexBuilder.scala
+++ graph-core/src/main/scala/ch/datascience/graph/elements/new_/build/NewVertexBuilder.scala
@@ -53,6 +53,13 @@ class NewVertexBuilder(val tempId: NewVertex#TempId) {
     addProperty(new DetachedSingleValueBuilder(key, value))
   }
 
+  def addSingleProperty(key: NewVertex#Prop#Key, value: NewVertex#Prop#Value, block: (DetachedRichPropertyBuilder) => Unit): this.type = {
+    val builder = new DetachedSingleValueBuilder(key, value)
+    addProperty(builder)
+    block(builder.property)
+    this
+  }
+
   def addSetProperty(key: NewVertex#Prop#Key): this.type = {
     addProperty(new DetachedSetValueBuilder(key))
   }
@@ -69,6 +76,22 @@ class NewVertexBuilder(val tempId: NewVertex#TempId) {
     this
   }
 
+  def addSetProperty(key: NewVertex#Prop#Key, value: NewVertex#Prop#Value, block: (DetachedRichPropertyBuilder) => Unit): this.type = {
+    if (!properties.contains(key)) {
+      addSetProperty(key: NewVertex#Prop#Key)
+    }
+    val multiProp = properties(key)
+    multiProp match {
+      case p: DetachedSetValueBuilder => {
+        val builder = new DetachedRichPropertyBuilder(key, value)
+        p.addProperty(builder)
+        block(builder)
+      }
+      case _ => throw new IllegalArgumentException(s"Bad cardinality, expected ${multiProp.cardinality} is not Set")
+    }
+    this
+  }
+
   def addListProperty(key: NewVertex#Prop#Key): this.type = {
     addProperty(new DetachedListValueBuilder(key))
   }
@@ -80,6 +103,22 @@ class NewVertexBuilder(val tempId: NewVertex#TempId) {
     val multiProp = properties(key)
     multiProp match {
       case p: DetachedListValueBuilder => p.addProperty(new DetachedRichPropertyBuilder(key, value))
+      case _ => throw new IllegalArgumentException(s"Bad cardinality, expected ${multiProp.cardinality} is not List")
+    }
+    this
+  }
+
+  def addListProperty(key: NewVertex#Prop#Key, value: NewVertex#Prop#Value, block: (DetachedRichPropertyBuilder) => Unit): this.type = {
+    if (!properties.contains(key)) {
+      addListProperty(key: NewVertex#Prop#Key)
+    }
+    val multiProp = properties(key)
+    multiProp match {
+      case p: DetachedListValueBuilder => {
+        val builder = new DetachedRichPropertyBuilder(key, value)
+        p.addProperty(builder)
+        block(builder)
+      }
       case _ => throw new IllegalArgumentException(s"Bad cardinality, expected ${multiProp.cardinality} is not List")
     }
     this
