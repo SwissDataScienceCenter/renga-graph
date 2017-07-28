0a5f0f93d436db54af025ade786fa64a90f20576
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-07-28 10:50:45 +0200

    graph-typesystem-service: reformatted

--- graph-typesystem-service/app/models/json/NamespaceAndNameMappers.scala
+++ graph-typesystem-service/app/models/json/NamespaceAndNameMappers.scala
@@ -22,20 +22,21 @@ import ch.datascience.graph.naming.NamespaceAndName
 import play.api.libs.json._
 
 /**
-  * Created by johann on 16/05/17.
-  */
+ * Created by johann on 16/05/17.
+ */
 object NamespaceAndNameMappers {
 
   def namespaceAndNameWrites: Writes[NamespaceAndName] = new Writes[NamespaceAndName] {
-    def writes(key: NamespaceAndName): JsString = JsString(key.asString)
+    def writes( key: NamespaceAndName ): JsString = JsString( key.asString )
   }
 
   def namespaceAndNameReads: Reads[NamespaceAndName] = new Reads[NamespaceAndName] {
-    def reads(json: JsValue): JsResult[NamespaceAndName] = json.validate[String] flatMap { str =>
+    def reads( json: JsValue ): JsResult[NamespaceAndName] = json.validate[String] flatMap { str =>
       try {
-        JsSuccess(NamespaceAndName(str))
-      } catch {
-        case e: IllegalArgumentException => JsError(e.getMessage)
+        JsSuccess( NamespaceAndName( str ) )
+      }
+      catch {
+        case e: IllegalArgumentException => JsError( e.getMessage )
       }
     }
   }
