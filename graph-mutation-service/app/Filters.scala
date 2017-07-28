26a0118a8da8b71176cbac2ebca8d02024b0099d
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-07-28 10:49:32 +0200

    graph-mutation-service: reformatted

--- graph-mutation-service/app/Filters.scala
+++ graph-mutation-service/app/Filters.scala
@@ -8,16 +8,16 @@ import play.filters.hosts.AllowedHostsFilter
 
 /**
  * Add the following filters by default to all projects
- * 
- * https://www.playframework.com/documentation/latest/ScalaCsrf 
+ *
+ * https://www.playframework.com/documentation/latest/ScalaCsrf
  * https://www.playframework.com/documentation/latest/AllowedHostsFilter
  * https://www.playframework.com/documentation/latest/SecurityHeaders
  */
 class Filters @Inject() (
-  allowedHostsFilter: AllowedHostsFilter,
-  securityHeadersFilter: SecurityHeadersFilter,
-  securityFilter: SecurityHeadersFilter,
-  corsFilter: CORSFilter
+    allowedHostsFilter:    AllowedHostsFilter,
+    securityHeadersFilter: SecurityHeadersFilter,
+    securityFilter:        SecurityHeadersFilter,
+    corsFilter:            CORSFilter
 ) extends DefaultHttpFilters(
   allowedHostsFilter,
   securityHeadersFilter,
