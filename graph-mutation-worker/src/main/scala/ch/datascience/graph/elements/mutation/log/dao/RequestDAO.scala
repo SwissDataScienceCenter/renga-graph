79a80240c38f2156d27d265e36b8eab3dd0d9100
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-07-28 10:49:52 +0200

    graph-mutation-worker: reformatted

--- graph-mutation-worker/src/main/scala/ch/datascience/graph/elements/mutation/log/dao/RequestDAO.scala
+++ graph-mutation-worker/src/main/scala/ch/datascience/graph/elements/mutation/log/dao/RequestDAO.scala
@@ -26,27 +26,27 @@ import play.api.libs.json.JsValue
 import slick.basic.DatabaseConfig
 import slick.jdbc.JdbcProfile
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ ExecutionContext, Future }
 
 /**
-  * Created by johann on 07/06/17.
-  */
+ * Created by johann on 07/06/17.
+ */
 class RequestDAO(
-  protected val ec: ExecutionContext,
-  protected val dbConfig: DatabaseConfig[JdbcProfile],
-  protected val dal: DatabaseStack
+    protected val ec:       ExecutionContext,
+    protected val dbConfig: DatabaseConfig[JdbcProfile],
+    protected val dal:      DatabaseStack
 ) extends DatabaseComponent {
 
   import profile.api._
 
   def all(): Future[Seq[Event]] = db.run( dal.requests.result )
 
-  def findByIdWithResponse(id: UUID): Future[Option[(Event, Option[Event])]] = {
-    db.run( dal.requests.findByIdWithResponse(id).result.headOption )
+  def findByIdWithResponse( id: UUID ): Future[Option[( Event, Option[Event] )]] = {
+    db.run( dal.requests.findByIdWithResponse( id ).result.headOption )
   }
 
-  def add(event: JsValue): Future[Event] = {
-    db.run( dal.requests.add(event)(ec) )
+  def add( event: JsValue ): Future[Event] = {
+    db.run( dal.requests.add( event )( ec ) )
   }
 
 }
--- graph-mutation-worker/src/main/scala/ch/datascience/graph/elements/mutation/worker/ResponseWorker.scala
+++ graph-mutation-worker/src/main/scala/ch/datascience/graph/elements/mutation/worker/ResponseWorker.scala
@@ -20,12 +20,12 @@ package ch.datascience.graph.elements.mutation.worker
 
 import java.util.UUID
 
-import ch.datascience.graph.elements.mutation.create.{CreateEdgeOperation, CreateVertexOperation, CreateVertexPropertyOperation}
+import ch.datascience.graph.elements.mutation.create.{ CreateEdgeOperation, CreateVertexOperation, CreateVertexPropertyOperation }
 import ch.datascience.graph.elements.mutation.json.MutationFormat
 import ch.datascience.graph.elements.mutation.log.dao.ResponseDAO
-import ch.datascience.graph.elements.mutation.tinkerpop_mappers.{CreateEdgeOperationMapper, CreateVertexOperationMapper, CreateVertexPropertyOperationMapper, UpdateVertexPropertyOperationMapper}
+import ch.datascience.graph.elements.mutation.tinkerpop_mappers.{ CreateEdgeOperationMapper, CreateVertexOperationMapper, CreateVertexPropertyOperationMapper, UpdateVertexPropertyOperationMapper }
 import ch.datascience.graph.elements.mutation.update.UpdateVertexPropertyOperation
-import ch.datascience.graph.elements.mutation.{Mutation, Operation}
+import ch.datascience.graph.elements.mutation.{ Mutation, Operation }
 import ch.datascience.graph.elements.new_.NewEdge
 import ch.datascience.graph.elements.persisted.PersistedVertex
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource
@@ -35,41 +35,41 @@ import play.api.libs.json._
 
 import scala.collection.mutable
 import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.{ Await, ExecutionContext, Future }
 
 /**
-  * Created by johann on 07/06/17.
-  */
+ * Created by johann on 07/06/17.
+ */
 class ResponseWorker(
-  protected val queue: Queue[(UUID, JsValue)],
-  protected val graph: JanusGraph,
-  protected val dao: ResponseDAO,
-  protected val ec: ExecutionContext
+    protected val queue: Queue[( UUID, JsValue )],
+    protected val graph: JanusGraph,
+    protected val dao:   ResponseDAO,
+    protected val ec:    ExecutionContext
 ) extends GraphComponent {
 
   start()
 
   def start(): Unit = {
-    Future.successful(()).map{ _ => this.work() }
+    Future.successful( () ).map { _ => this.work() }
   }
 
   def work(): Unit = {
-    while (queue.nonEmpty) {
-      val (requestId, event) = queue.dequeue
+    while ( queue.nonEmpty ) {
+      val ( requestId, event ) = queue.dequeue
       try {
-        processOneEvent(requestId, event)
+        processOneEvent( requestId, event )
       }
       catch {
         case e: Throwable =>
-          println(s"Unhandled exception: $e")
+          println( s"Unhandled exception: $e" )
           e.printStackTrace()
       }
     }
-    queue.register().future.map{ _ => this.work() }
+    queue.register().future.map { _ => this.work() }
   }
 
-  def processOneEvent(requestId:UUID, event: JsValue): Unit = {
-    val mutation = (event \ "query").as[Mutation](MutationFormat)
+  def processOneEvent( requestId: UUID, event: JsValue ): Unit = {
+    val mutation = ( event \ "query" ).as[Mutation]( MutationFormat )
     val operations = mutation.operations
 
     // tempId -> persistedId map
@@ -81,44 +81,43 @@ class ResponseWorker(
       execute {
         val results = for {
           op <- operations
-        } yield processOperation(g, op, idMap)
+        } yield processOperation( g, op, idMap )
 
-        val json = JsObject(Seq("status" -> JsString("success"), "results" -> JsArray(results)))
+        val json = JsObject( Seq( "status" -> JsString( "success" ), "results" -> JsArray( results ) ) )
 
         //      println(s"TODO: push to response db: $json")
-        val logResponse = dao.add(requestId, json)
-        Await.result(logResponse, Duration.Inf)
+        val logResponse = dao.add( requestId, json )
+        Await.result( logResponse, Duration.Inf )
       }
     }
     catch {
       case e: Throwable =>
-        println(s"Request failed")
-        dao.add(requestId, JsObject(Seq("status" -> JsString("failed"), "reason" -> JsString(e.getMessage))))
+        println( s"Request failed" )
+        dao.add( requestId, JsObject( Seq( "status" -> JsString( "failed" ), "reason" -> JsString( e.getMessage ) ) ) )
         throw e
     }
 
-
   }
 
-  def processOperation(g: GraphTraversalSource, op: Operation, idMap: mutable.Map[NewEdge#NewVertexType#TempId, NewEdge#PersistedVertexType#Id]): JsValue = op match {
+  def processOperation( g: GraphTraversalSource, op: Operation, idMap: mutable.Map[NewEdge#NewVertexType#TempId, NewEdge#PersistedVertexType#Id] ): JsValue = op match {
     case o: CreateVertexOperation =>
-      val vertex = CreateVertexOperationMapper(o)(g).next()
+      val vertex = CreateVertexOperationMapper( o )( g ).next()
       val id = vertex.id().asInstanceOf[PersistedVertex#Id]
       idMap += o.vertex.tempId -> id
-      JsObject(Seq("id" -> JsNumber(id)))
+      JsObject( Seq( "id" -> JsNumber( id ) ) )
     case o: CreateEdgeOperation =>
-      val edge = CreateEdgeOperationMapper(idMap.toMap)(o)(g).next()
+      val edge = CreateEdgeOperationMapper( idMap.toMap )( o )( g ).next()
       val id = edge.id().asInstanceOf[RelationIdentifier].toString
-      JsObject(Seq("id" -> JsString(id)))
+      JsObject( Seq( "id" -> JsString( id ) ) )
     case o: CreateVertexPropertyOperation =>
-      val vertex = CreateVertexPropertyOperationMapper(o)(g).next()
+      val vertex = CreateVertexPropertyOperationMapper( o )( g ).next()
       val id = vertex.id().asInstanceOf[PersistedVertex#Id]
-      JsObject(Seq("id" -> JsNumber(id)))
+      JsObject( Seq( "id" -> JsNumber( id ) ) )
     case o: UpdateVertexPropertyOperation =>
-      val vertex = UpdateVertexPropertyOperationMapper(o)(g).next()
+      val vertex = UpdateVertexPropertyOperationMapper( o )( g ).next()
       val id = vertex.id().asInstanceOf[PersistedVertex#Id]
-      JsObject(Seq("id" -> JsNumber(id)))
-    case o => throw new IllegalArgumentException(s"Unsupported operation: $o")
+      JsObject( Seq( "id" -> JsNumber( id ) ) )
+    case o => throw new IllegalArgumentException( s"Unsupported operation: $o" )
   }
 
   private[this] implicit lazy val e: ExecutionContext = ec
