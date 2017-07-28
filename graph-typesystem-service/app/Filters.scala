cb45e195985f0e2f6b56637226eea4d48588a983
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-07-28 10:50:45 +0200

    graph-typesystem-service: reformatted

--- graph-typesystem-service/app/Filters.scala
+++ graph-typesystem-service/app/Filters.scala
@@ -6,17 +6,17 @@ import play.filters.headers.SecurityHeadersFilter
 import play.filters.hosts.AllowedHostsFilter
 
 /**
-  * Add the following filters by default to all projects
-  *
-  * https://www.playframework.com/documentation/latest/ScalaCsrf
-  * https://www.playframework.com/documentation/latest/AllowedHostsFilter
-  * https://www.playframework.com/documentation/latest/SecurityHeaders
-  */
+ * Add the following filters by default to all projects
+ *
+ * https://www.playframework.com/documentation/latest/ScalaCsrf
+ * https://www.playframework.com/documentation/latest/AllowedHostsFilter
+ * https://www.playframework.com/documentation/latest/SecurityHeaders
+ */
 class Filters @Inject() (
-  allowedHostsFilter: AllowedHostsFilter,
-  securityHeadersFilter: SecurityHeadersFilter,
-  securityFilter: SecurityHeadersFilter,
-  corsFilter: CORSFilter
+    allowedHostsFilter:    AllowedHostsFilter,
+    securityHeadersFilter: SecurityHeadersFilter,
+    securityFilter:        SecurityHeadersFilter,
+    corsFilter:            CORSFilter
 ) extends DefaultHttpFilters(
   allowedHostsFilter,
   securityHeadersFilter,
