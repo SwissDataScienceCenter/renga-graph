40315347ade3a59495b11c2862f098da33ed6abd
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-07-28 10:50:24 +0200

    graph-typesystem-persistence: reformatted

--- graph-typesystem-persistence/src/main/scala/ch/datascience/graph/types/persistence/model/Transition.scala
+++ graph-typesystem-persistence/src/main/scala/ch/datascience/graph/types/persistence/model/Transition.scala
@@ -22,6 +22,6 @@ import java.time.Instant
 import java.util.UUID
 
 /**
-  * Created by johann on 17/03/17.
-  */
-case class Transition(entityId: UUID, from: Long, toState: EntityState, toTimestamp: Instant) extends Row
+ * Created by johann on 17/03/17.
+ */
+case class Transition( entityId: UUID, from: Long, toState: EntityState, toTimestamp: Instant ) extends Row
--- graph-typesystem-persistence/src/main/scala/ch/datascience/graph/types/persistence/model/json/GraphDomainMappers.scala
+++ graph-typesystem-persistence/src/main/scala/ch/datascience/graph/types/persistence/model/json/GraphDomainMappers.scala
@@ -26,21 +26,21 @@ import play.api.libs.functional.syntax._
 import play.api.libs.json._
 
 /**
-  * Created by johann on 13/06/17.
-  */
+ * Created by johann on 13/06/17.
+ */
 object GraphDomainMappers {
 
   lazy val GraphDomainFormat: Format[GraphDomain] = (
-    (JsPath \ "id").format[UUID](UUIDFormat) and
-      (JsPath \ "namespace").format[String](NamespaceFormat)
-  )(GraphDomain.apply, unlift(GraphDomain.unapply))
+    ( JsPath \ "id" ).format[UUID]( UUIDFormat ) and
+    ( JsPath \ "namespace" ).format[String]( NamespaceFormat )
+  )( GraphDomain.apply, unlift( GraphDomain.unapply ) )
 
-  lazy val GraphDomainRequestFormat: Format[String] = Format(graphDomainRequestReads, (JsPath \ "namespace").write[String])
+  lazy val GraphDomainRequestFormat: Format[String] = Format( graphDomainRequestReads, ( JsPath \ "namespace" ).write[String] )
 
   private[this] def graphDomainRequestReads: Reads[String] = Reads { json =>
-    (JsPath \ "namespace").read[String](NamespaceFormat).reads(json) match {
-      case JsSuccess(ns, _) => JsSuccess(ns)
-      case e: JsError => e
+    ( JsPath \ "namespace" ).read[String]( NamespaceFormat ).reads( json ) match {
+      case JsSuccess( ns, _ ) => JsSuccess( ns )
+      case e: JsError         => e
     }
   }
 
