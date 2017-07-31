28dabe95d33a4603984bd983e4060f58cdb9dcb9
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-07-31 08:52:15 +0200

    graph-typesystem-persistence: formatted test sources

--- graph-typesystem-persistence/src/test/scala/ch/datascience/graph/types/persistence/AsyncUnitSpec.scala
+++ graph-typesystem-persistence/src/test/scala/ch/datascience/graph/types/persistence/AsyncUnitSpec.scala
@@ -18,11 +18,11 @@
 
 package ch.datascience.graph.types.persistence
 
-import org.scalatest.{AsyncFlatSpec, Matchers}
+import org.scalatest.{ AsyncFlatSpec, Matchers }
 
 /**
-  * Created by johann on 10/04/17.
-  */
+ * Created by johann on 10/04/17.
+ */
 abstract class AsyncUnitSpec extends AsyncFlatSpec with Matchers {
 
 }
--- graph-typesystem-persistence/src/test/scala/ch/datascience/graph/types/persistence/DatabaseSetup.scala
+++ graph-typesystem-persistence/src/test/scala/ch/datascience/graph/types/persistence/DatabaseSetup.scala
@@ -19,7 +19,7 @@
 package ch.datascience.graph.types.persistence
 
 import ch.datascience.graph.types.persistence.relationaldb.DatabaseStack
-import org.scalatest.{BeforeAndAfterAll, Suite}
+import org.scalatest.{ BeforeAndAfterAll, Suite }
 import play.api.db.slick.HasDatabaseConfig
 import slick.basic.DatabaseConfig
 import slick.jdbc.JdbcProfile
@@ -28,30 +28,30 @@ import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
 /**
-  * Created by johann on 13/04/17.
-  */
+ * Created by johann on 13/04/17.
+ */
 //trait DatabaseSetup extends BeforeAndAfterAll with DatabaseConfigComponent[JdbcProfile] { this : Suite =>
-trait DatabaseSetup extends BeforeAndAfterAll with HasDatabaseConfig[JdbcProfile] { this : Suite =>
+trait DatabaseSetup extends BeforeAndAfterAll with HasDatabaseConfig[JdbcProfile] { this: Suite =>
 
   import profile.api._
 
-  val dbConfig: DatabaseConfig[JdbcProfile] = DatabaseConfig.forConfig[JdbcProfile]("db-test")
+  val dbConfig: DatabaseConfig[JdbcProfile] = DatabaseConfig.forConfig[JdbcProfile]( "db-test" )
 
-//  val profile: JdbcProfile = dbConfig.profile
-//
-//  import profile.api._
-//  val db: Database = Database.forConfig("", config = dbConfig.config)
-//
-//  class DatabaseStackTest(val profile: JdbcProfile) extends DatabaseStack
-//
-//  val dal = new DatabaseStackTest(profile)
+  //  val profile: JdbcProfile = dbConfig.profile
+  //
+  //  import profile.api._
+  //  val db: Database = Database.forConfig("", config = dbConfig.config)
+  //
+  //  class DatabaseStackTest(val profile: JdbcProfile) extends DatabaseStack
+  //
+  //  val dal = new DatabaseStackTest(profile)
 
-  val dal = new DatabaseStack(dbConfig)
+  val dal = new DatabaseStack( dbConfig )
 
   override protected def beforeAll(): Unit = {
-    val createSchemas: DBIO[Unit] = dal.schemas.map(_.asInstanceOf[profile.SchemaDescription]).reduce((x, y) => x ++ y).create
-    val run = db.run(createSchemas)
-    Await.result(run, Duration.Inf)
+    val createSchemas: DBIO[Unit] = dal.schemas.map( _.asInstanceOf[profile.SchemaDescription] ).reduce( ( x, y ) => x ++ y ).create
+    val run = db.run( createSchemas )
+    Await.result( run, Duration.Inf )
 
     super.beforeAll()
   }
@@ -59,9 +59,9 @@ trait DatabaseSetup extends BeforeAndAfterAll with HasDatabaseConfig[JdbcProfile
   override protected def afterAll(): Unit = {
     try super.afterAll()
     finally {
-      val deleteSchemas: DBIO[Unit] = dal.schemas.map(_.asInstanceOf[profile.SchemaDescription]).reduce((x,y) => x ++ y).drop
-      val run = db.run(deleteSchemas)
-      Await.result(run, Duration.Inf)
+      val deleteSchemas: DBIO[Unit] = dal.schemas.map( _.asInstanceOf[profile.SchemaDescription] ).reduce( ( x, y ) => x ++ y ).drop
+      val run = db.run( deleteSchemas )
+      Await.result( run, Duration.Inf )
     }
   }
 
