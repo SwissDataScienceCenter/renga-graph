d33aec8b042debde422df6bf060d86d447145226
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-08-22 10:16:02 +0200

    Trying a real deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-echo $1
-# sbt ++$TRAVIS_SCALA_VERSION "project $component" publish
+sbt ++$TRAVIS_SCALA_VERSION "project $COMPONENT" publish
+
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" $DOCKER_REGISTRY
+sbt ++$TRAVIS_SCALA_VERSION "project $COMPONENT" docker:publishLocal
+docker tag renga-graph-$COMPONENT:0.1.0-SNAPSHOT $DOCKER_REGISTRY/swissdatasciencecenter/images/renga-graph-$COMPONENT:0.1.0-SNAPSHOT
+docker push $DOCKER_REGISTRY/swissdatasciencecenter/images/renga-graph-$COMPONENT:0.1.0-SNAPSHOT
