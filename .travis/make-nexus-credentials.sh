88326cd3293c27c34fc49e14ed8d2cd35a69c8b9
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-08-22 09:59:56 +0200

    Trying build with deploy only on master

--- .travis/make-nexus-credentials.sh
+++ .travis/make-nexus-credentials.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-CREDENTIALS="$HOME/.ivy2/.credentials"
+CREDENTIALS_DIR="$HOME/.ivy2"
+mkdir -p "$CREDENTIALS_DIR"
+
+CREDENTIALS="$CREDENTIALS_DIR/.credentials"
 echo "realm=Sonatype Nexus Repository Manager" > $CREDENTIALS
 echo "host=$NEXUS_HOST" >> $CREDENTIALS
 echo "user=$NEXUS_USER" >> $CREDENTIALS
