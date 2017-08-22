7df140ecb4cb68af26f499667542ee0c141c8937
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-08-22 10:33:45 +0200

    Build docker only on specified projects

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,7 +2,10 @@
 
 sbt ++$TRAVIS_SCALA_VERSION "project $COMPONENT" publish
 
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" $DOCKER_REGISTRY
-sbt ++$TRAVIS_SCALA_VERSION "project $COMPONENT" docker:publishLocal
-docker tag renga-graph-$COMPONENT:0.1.0-SNAPSHOT $DOCKER_REGISTRY/swissdatasciencecenter/images/renga-graph-$COMPONENT:0.1.0-SNAPSHOT
-docker push $DOCKER_REGISTRY/swissdatasciencecenter/images/renga-graph-$COMPONENT:0.1.0-SNAPSHOT
+case $COMPONENT in
+  init|mutation-service|navigation-service|typesystem-service)
+    docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" $DOCKER_REGISTRY
+    sbt ++$TRAVIS_SCALA_VERSION "project $COMPONENT" docker:publishLocal
+    docker tag renga-graph-$COMPONENT:0.1.0-SNAPSHOT $DOCKER_REGISTRY/swissdatasciencecenter/images/renga-graph-$COMPONENT:0.1.0-SNAPSHOT
+    docker push $DOCKER_REGISTRY/swissdatasciencecenter/images/renga-graph-$COMPONENT:0.1.0-SNAPSHOT ;;
+esac
