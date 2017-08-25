197da1d089514ffb93f362c187c05b3ea99cf24f
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-08-25 15:32:27 +0200

    Travis may deploy to latest

---
+++ .travis/deploy-docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+case $COMPONENT in
+  init|mutation-service|navigation-service|typesystem-service)
+    docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" $DOCKER_REGISTRY
+    sbt ++$TRAVIS_SCALA_VERSION \
+      "project $COMPONENT"
+      "set dockerRepository := scala.sys.env.get(\"DOCKER_REGISTRY\")" \
+      "set dockerUsername := scala.sys.env.get(\"DOCKER_REPOSITORY\")" \
+      "set dockerUpdateLatest := ( scala.sys.env.getOrElse(\"DOCKER_PUSH_LATEST\", \"\").toLowerCase == \"true\" )" \
+      "docker:publish" ;;
+esac
